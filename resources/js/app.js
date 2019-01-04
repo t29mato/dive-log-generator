9da61b81db149fe099aabcd5d7a5a5b89ae58a66
mato <tomoya.matou@gmail.com>
2019-01-04 10:06:50 +0900

    create realtime dive time js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -6,30 +5,36 @@
  */
 
 require('./bootstrap');
+var moment = require('moment');
 
-// calculate dive time on realtime.
-$("#timeEntry").on('click', function() {
+/**
+ * Calculate dive time on realtime.
+ */
+
+$("#timeEntry").on('click change', function () {
+    calDiveTime();
+});
+$("#timeExit").on('click change', function () {
     calDiveTime();
 });
 
-var calDiveTime = function() {
-    var $timeEntry = $('#timeEntry').val();
-    var $timeExit = $('#timeExit').val();
-    if (isset($timeEntry) && isset($timeExit)) {
-        var timeDive = $timeExit - $timeEntry;
-        var result2 = timeMath.sub('12:34', '00:12');
-        console.log(result2);
-        console.log($timeExit);
-        console.log($timeEntry);
-        console.log(timeDive);
+var calDiveTime = function () {
+    var timeEntry = moment('2019-01-01 ' + $('#timeEntry').val());
+    var timeExit = moment('2019-01-01 ' + $('#timeExit').val());
+    if (isset(timeEntry) && isset(timeExit)) {
+        var timeDive = timeExit.diff(timeEntry, 'minutes');
         $('#timeDive').val(timeDive);
     }
 }
 
-var isset = function(data){
-    if(data === "" || data === null || data === undefined){
+/**
+ * isset function
+ */
+
+var isset = function (data) {
+    if (data === "" || data === null || data === undefined) {
         return false;
-    }else{
+    } else {
         return true;
     }
 };
