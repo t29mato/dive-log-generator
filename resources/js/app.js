45e7ac6d6fc4817d61fc24c0aa6127bded192ed7
mato <tomoya.matou@gmail.com>
2019-01-04 09:20:39 +0900

    create index page

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,27 +7,29 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app'
+// calculate dive time on realtime.
+$("#timeEntry").on('click', function() {
+    calDiveTime();
 });
+
+var calDiveTime = function() {
+    var $timeEntry = $('#timeEntry').val();
+    var $timeExit = $('#timeExit').val();
+    if (isset($timeEntry) && isset($timeExit)) {
+        var timeDive = $timeExit - $timeEntry;
+        var result2 = timeMath.sub('12:34', '00:12');
+        console.log(result2);
+        console.log($timeExit);
+        console.log($timeEntry);
+        console.log(timeDive);
+        $('#timeDive').val(timeDive);
+    }
+}
+
+var isset = function(data){
+    if(data === "" || data === null || data === undefined){
+        return false;
+    }else{
+        return true;
+    }
+};
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,93 @@
 @extends('common')
 
 @section('content')
-コンテンツ
+<form>
+    <table class="table-dive-diagram">
+        <tbody>
+            <tr>
+                <td class="border-dark border-bottom">
+                    <label>Entry Time</label>
+                    <div class="input-group">
+                        <input tabindex="1" type="time" class="form-control" id="timeEntry" value="10:00">
+                    </div>
+                </td>
+                <td>
+                    <label>Dive Time</label>
+                    <div class="input-group">
+                        <input type="text" class="form-control" id="timeDive" disabled>
+                        <div class="input-group-append">
+                            <span class="input-group-text">min</span>
+                        </div>
+                    </div>
+                </td>
+                <td class="border-dark border-bottom">
+                    <label>Exit Time</label>
+                    <div class="input-group">
+                        <input tabindex="2" type="time" class="form-control" id="timeExit" value="10:00">
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label>Top Temp.</label>
+                    <div class="input-group">
+                        <input tabindex="3" type="number" class="form-control" id="tempTop" value="20" min="0" max="40">
+                        <div class="input-group-append">
+                            <span class="input-group-text">℃</span>
+                        </div>
+                    </div>
+                </td>
+                <td class="border-dark border-left border-right">
+                    <label>Average Depth</label>
+                    <div class="input-group">
+                        <input tabindex="5" type="number" class="form-control" id="tempTop" value="10.0" min="1" max="40" step="0.1">
+                        <div class="input-group-append">
+                            <span class="input-group-text">m</span>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <label>Entry Pressure</label>
+                    <div class="input-group">
+                        <input tabindex="7" type="number" class="form-control" id="tempTop" value="20" min="0" max="40">
+                        <div class="input-group-append">
+                            <span class="input-group-text">atm</span>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label>Bottom Temp.</label>
+                    <div class="input-group">
+                        <input tabindex="4" type="number" class="form-control" id="tempTop" value="20" min="0" max="40">
+                        <div class="input-group-append">
+                            <span class="input-group-text">℃</span>
+                        </div>
+                    </div>
+                </td>
+                <td class="border-dark border-left border-right border-bottom">
+                    <label>Max Depth</label>
+                    <div class="input-group">
+                        <input tabindex="6" type="number" class="form-control" id="tempTop" value="10.0" min="1" max="40" step="0.1">
+                        <div class="input-group-append">
+                            <span class="input-group-text">m</span>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <label>Exit Pressure</label>
+                    <div class="input-group">
+                        <input tabindex="8" type="number" class="form-control" id="tempTop" value="20" min="0" max="40">
+                        <div class="input-group-append">
+                            <span class="input-group-text">atm</span>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 @endsection
