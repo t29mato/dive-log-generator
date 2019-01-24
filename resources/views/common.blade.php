107a1d1857f69c1748df40e69c7fd8759af17da7
mato <tomoya.matou@gmail.com>
2019-01-25 00:09:12 +0900

    rmove humbremenu for SP design

--- resources/views/common.blade.php
+++ resources/views/common.blade.php
@@ -28,7 +28,8 @@
                 {{ config('app.name', 'Laravel') }}
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}"
+                style="display:none;">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
