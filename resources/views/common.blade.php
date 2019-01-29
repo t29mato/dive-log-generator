fbfb4a51575e9c33e7c38aca005fdd668ca62fd1
mato <tomoya.matou@gmail.com>
2019-01-30 01:10:21 +0900

    add humberger menu for sp

--- resources/views/common.blade.php
+++ resources/views/common.blade.php
@@ -38,8 +38,7 @@
                 <img src="{{ asset('images/font-umineco.png') }}" height="20" />
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}"
-                style="display:none;">
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
